d5f1056988bfef04b595982b6a0437e27c890efe
Arinto Murdopo <arinto@yahoo-inc.com>
2013-02-18 11:36:51 +0100

    Add exponential forgetting word count. Time is not taken into account

--- src/main/java/com/yahoo/swc/TwitterWordCountTopology.java
+++ src/main/java/com/yahoo/swc/TwitterWordCountTopology.java
@@ -1,48 +1,103 @@
 package com.yahoo.swc;
 
+import java.util.HashMap;
+
 import com.yahoo.swc.bolt.CountWord;
+import com.yahoo.swc.bolt.EfCountWord;
 import com.yahoo.swc.bolt.SplitTweet;
 import com.yahoo.swc.spout.TwitterSampleSpout;
 
 import backtype.storm.Config;
 import backtype.storm.LocalCluster;
+import backtype.storm.StormSubmitter;
+import backtype.storm.generated.AlreadyAliveException;
+import backtype.storm.generated.InvalidTopologyException;
 import backtype.storm.topology.TopologyBuilder;
+import backtype.storm.topology.base.BaseRichBolt;
 import backtype.storm.tuple.Fields;
 import backtype.storm.utils.Utils;
 
 public class TwitterWordCountTopology {
+	
+	private static final String NORMAL_MODE = "nrml";
+	private static final String EF_MODE = "ef";
+	
+	private static final HashMap<String, String> ALLOWED_OPTION = new HashMap<String, String>() {
+		private static final long serialVersionUID = -4689033609960231840L;
+
+		{
+			put(NORMAL_MODE, "for normal counting");
+			put(EF_MODE, "for exponential forgetting counter");
+		}
+	};
+		
 
 	/**
 	 * @param args
+	 * @throws InvalidTopologyException 
+	 * @throws AlreadyAliveException 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws AlreadyAliveException, InvalidTopologyException {
+		
+		if(args.length < 1 || !ALLOWED_OPTION.containsKey(args[0])){
+			System.out.println("Usage \"TwitterWordCountTopology CountingMode\"");
+			System.out.println("Where CountingMode is: ");
+			for(String key: ALLOWED_OPTION.keySet()){
+				System.out.println(key + ", " + ALLOWED_OPTION.get(key));
+			}
+		}
 		
 		TopologyBuilder builder = new TopologyBuilder();
 		
 		//use TwitterSampleSpout
 		String twitterSampleSpoutId = "sample_tweet";
-		builder.setSpout(twitterSampleSpoutId, new TwitterSampleSpout(), 1);
+		int twitterSampleSpoutParallelism = 1;
+		builder.setSpout(twitterSampleSpoutId, new TwitterSampleSpout(), twitterSampleSpoutParallelism);
 		
 		//use SplitTweet Bolt
 		String splitTweetBoltId = "split_tweet";
-		builder.setBolt(splitTweetBoltId, new SplitTweet(), 10)
+		int splitTweetParallelism = 10;
+		builder.setBolt(splitTweetBoltId, new SplitTweet(), splitTweetParallelism)
 				.shuffleGrouping(twitterSampleSpoutId);
 			
 		//use CountWord Bolt
-		String countWordBoltId = "count_word";
-		builder.setBolt(countWordBoltId, new CountWord(), 20)
+		//better code: use builder class :)
+		BaseRichBolt countWordBolt = null;
+		String countWordBoltId = null;
+		if(args[0].equals(NORMAL_MODE)){
+			countWordBolt = new CountWord();
+			countWordBoltId = "count_word";
+		}else if (args[0].equals(EF_MODE)){
+			countWordBolt = new EfCountWord();
+			countWordBoltId = "ef_count_word";
+		}else{
+			System.out.println(args[0] + " is a valid option but not yet implemented");
+			return;
+		}
+
+		int countWordBoltParallelism = 20;
+		builder.setBolt(countWordBoltId, countWordBolt, countWordBoltParallelism)
 				.fieldsGrouping(splitTweetBoltId, new Fields("word"));
 		
 		Config conf = new Config();
 		conf.setDebug(true);
 		
-		//launch the topology in LocalCluster
-		String topologyName = "word_count_topology";
-		LocalCluster cluster = new LocalCluster();
-		cluster.submitTopology(topologyName , conf, builder.createTopology());
-		Utils.sleep(10000);
-		cluster.killTopology(topologyName);
-		cluster.shutdown();
+		if(args != null && args.length > 1){
+			conf.setNumWorkers(3);
+			
+			StormSubmitter.submitTopology(args[1], conf, builder.createTopology());
+		}else{
+			conf.setMaxTaskParallelism(3);
+			
+			String topologyName = "word_count_topology";
+			LocalCluster cluster = new LocalCluster();
+			cluster.submitTopology(topologyName , conf, builder.createTopology());
+			
+			Utils.sleep(10000);
+			
+			cluster.killTopology(topologyName);
+			cluster.shutdown();			
+		}
 	}
 
 }
--- src/main/java/com/yahoo/swc/bolt/EfCountWord.java
+++ src/main/java/com/yahoo/swc/bolt/EfCountWord.java
@@ -1,5 +1,6 @@
 package com.yahoo.swc.bolt;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import backtype.storm.task.OutputCollector;
@@ -10,35 +11,34 @@ import backtype.storm.tuple.Fields;
 import backtype.storm.tuple.Tuple;
 import backtype.storm.tuple.Values;
 
-public class SplitTweet extends BaseRichBolt {
-
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 3046647784643054989L;
-	private static final String REGEX_SPLIT = 
-			"[,;\\s\":'<>()\\[\\]\\^\\!\\$\\%\\&\\*\\-" +
-			"\\+=_\\{\\}\\.\\?\\\\/]+";
+public class EfCountWord extends BaseRichBolt {
+	
+	private static final long serialVersionUID = -7650439263074510377L;
 	
 	private OutputCollector _outputCollector = null;
+	private HashMap<String, Double> _efCounter = null;
+	private double _alpha = 0.1;
 	
 	public void prepare(@SuppressWarnings("rawtypes") Map stormConf, TopologyContext context,
 			OutputCollector collector) {
 		_outputCollector = collector;
-		
+		_efCounter = new HashMap<String, Double>();
 	}
 
-	public void execute(Tuple tuple) {
-		String tweetStatus = tuple.getString(0);
-		String[] words = tweetStatus.split(REGEX_SPLIT);
-		
-		for(String word: words){
-			_outputCollector.emit(new Values(word));
+	public void execute(Tuple input) {
+		//Exponential forgetting
+		String word = input.getString(0);
+		Double efCount = _efCounter.get(word);
+		if(efCount == null){
+			efCount = 0.0;
 		}
+		efCount = 1.0 + (1.0 - _alpha)*efCount;
+		_efCounter.put(word, efCount);
+		_outputCollector.emit(new Values(word, efCount));
 	}
 
 	public void declareOutputFields(OutputFieldsDeclarer declarer) {
-		declarer.declare(new Fields("word"));
+		declarer.declare(new Fields("word", "efcount"));		
 	}
 
 }
